5da19e6943380a9b292c3fe38c9168f569afbccc
Romain Lespinasse <romain.lespinasse@gmail.com>
2022-06-15 15:56:14 +0200

    use variable content

--- slugify.sh
+++ slugify.sh
@@ -51,8 +51,8 @@ reduce() {
   if [ "${MAX_LENGTH}" != "nolimit" ]; then
     reduced_value=$(cut -c1-"${MAX_LENGTH}" <<<"$1")
   fi
-  if [ "remove_ending_hypen" == "true" ]; then
-    reduced_value=$(sed -E 's/-*$//' <<<"$reduced_value")
+  if [ "$remove_ending_hypen" == "true" ]; then
+    reduced_value="${reduced_value//-*$/}"
   fi
   echo "$reduced_value"
 }
